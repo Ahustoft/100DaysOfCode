3d42520b7c0846a70927715f2f66ffbb2924efa1
Ahustoft <ahustoft@gmail.com>
2019-04-26 20:26:36 +0200

    Adds incremental game files

---
+++ incremental-game/js/game.js
@@ -0,0 +1,89 @@
+var cats = 0;
+
+function catClick(number) {
+  cats = cats + number;
+  document.getElementById("cats").innerHTML = cats;
+}
+
+var cheeseburger = 0;
+var nextburgerCost = 0;
+
+function buyCheeseburger() {
+  var cheeseburgerCost = Math.floor(10 * Math.pow(1.2, cheeseburger));
+  if (cats >= cheeseburgerCost) {
+    cheeseburger = cheeseburger + 1;
+    cats = cats - cheeseburgerCost;
+    document.getElementById("cheeseburger").innerHTML = cheeseburger;
+    document.getElementById("cats").innerHTML = cats;
+  }
+  nextburgerCost = Math.floor(10 * Math.pow(1.2, cheeseburger));
+  document.getElementById("cheeseburgerCost").innerHTML = nextburgerCost;
+}
+
+window.setInterval(function() {
+  catClick(cheeseburger);
+}, 1000);
+
+var mouse = 0;
+var nextmouseCost = 0;
+
+function buyMouse() {
+  var mouseCost = Math.floor(150 * Math.pow(1.4, mouse));
+  if (cats >= mouseCost) {
+    mouse = mouse + 1;
+    cats = cats - mouseCost;
+    document.getElementById("mouse").innerHTML = mouse;
+    document.getElementById("cats").innerHTML = cats;
+  }
+  nextmouseCost = Math.floor(150 * Math.pow(1.4, mouse));
+  document.getElementById("mouseCost").innerHTML = nextmouseCost;
+}
+
+window.setInterval(function() {
+  catClick(mouse * 5);
+}, 1000);
+
+function save() {
+  localStorage.setItem("cats", JSON.stringify(cats));
+  localStorage.setItem("cheeseburger", JSON.stringify(cheeseburger));
+  localStorage.setItem("cheeseburgerCost", JSON.stringify(cheeseburgerCost));
+  localStorage.setItem("mouse", JSON.stringify(mouse));
+  localStorage.setItem("mouseCost", JSON.stringify(mouseCost));
+  localStorage.setItem("nextburgerCost", JSON.stringify(nextburgerCost));
+  localStorage.setItem("nextmouseCost", JSON.stringify(nextmouseCost));
+}
+
+function load() {
+  cats = JSON.parse(localStorage.getItem("cats"));
+  cheeseburger = JSON.parse(localStorage.getItem("cheeseburger"));
+  cheeseburgerCost = JSON.parse(localStorage.getItem("cheeseburgerCost"));
+  mouse = JSON.parse(localStorage.getItem("mouse"));
+  mouseCost = JSON.parse(localStorage.getItem("mouseCost"));
+  nextCost = JSON.parse(localStorage.getItem("nextburgerCost"));
+  nextCost = JSON.parse(localStorage.getItem("nextmouseCost"));
+  document.getElementById("cats").innerHTML = JSON.parse(
+    localStorage.getItem("cats")
+  );
+  document.getElementById("cheeseburger").innerHTML = JSON.parse(
+    localStorage.getItem("cheeseburger")
+  );
+  document.getElementById("cheeseburgerCost").innerHTML = JSON.parse(
+    localStorage.getItem("nextburgerCost")
+  );
+  document.getElementById("mouse").innerHTML = JSON.parse(
+    localStorage.getItem("mouse")
+  );
+  document.getElementById("mouseCost").innerHTML = JSON.parse(
+    localStorage.getItem("nextmouseCost")
+  );
+}
+
+function del() {
+  localStorage.removeItem("cats");
+  localStorage.removeItem("cheeseburger");
+  localStorage.removeItem("cheeseburgerCost");
+  localStorage.removeItem("mouse");
+  localStorage.removeItem("mouseCost");
+  localStorage.removeItem("nextburgerCost");
+  localStorage.removeItem("nextmouseCost");
+}
